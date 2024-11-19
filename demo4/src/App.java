5e064660954713cce71f58eef1438e6245921293
tlappa12321 <theofaniaslappas123@gmail.com>
2024-11-19 09:32:52 -0600

    just the demo we did in class

--- demo4/src/App.java
+++ demo4/src/App.java
@@ -1,15 +1,12 @@
+import javax.print.attribute.Size2DSyntax;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] scores = null;
-        String[] names = null;
-
-        scores = new int[10];
-        names = new String [10];
-
-        names[0] = "wolf";
-        scores[0] = 9000;
+        System.out.println("how many ");
+        nums = new int[Size];
         
-        System.out.println(scores[0]);
+
+  
 
     }
 }
